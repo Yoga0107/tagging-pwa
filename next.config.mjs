9c5b12dfe65c6344f648381dc6294e3624261924
yogaputra0107 <yogaputra0107@gmail.com>
2025-11-15 20:27:16 +0700

    fix pwa

--- next.config.mjs
+++ next.config.mjs
@@ -11,5 +11,6 @@ export default withPWA({
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
+    buildExcludes: [/app-build-manifest\.json$/],
   },
 })
