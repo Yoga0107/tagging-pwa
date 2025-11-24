e232dca7b9741518e81c36eb3192be6ee1e1ebe2
yogaputra0107 <yogaputra0107@gmail.com>
2025-11-24 12:04:22 +0700

    rename logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import type { ReactNode } from 'react'
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata = {
-  title: 'My PWA App',
-  description: 'Next.js 14 PWA starter',
+  title: 'CPP - Digital Production',
+  description: 'PMS, Tagging System',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
